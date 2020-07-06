bb525294d82cde61b78c16d728cdacb25237994e
Shrikant Dande <shriiikant@gmail.com>
2020-07-07 00:37:42 +0530

    react spring war packaging

---
+++ react-and-spring-war/frontend/build/precache-manifest.b79df6283b221eb0a773b73a57a82061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f2a1b92d83d8a8bd02c22ac2b55efa",
+    "url": "/spring-and-react/index.html"
+  },
+  {
+    "revision": "a93a095fb9355b67f7db",
+    "url": "/spring-and-react/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "531b36856d98405b28f4",
+    "url": "/spring-and-react/static/js/2.5e0613e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spring-and-react/static/js/2.5e0613e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a93a095fb9355b67f7db",
+    "url": "/spring-and-react/static/js/main.345a3496.chunk.js"
+  },
+  {
+    "revision": "08cefe30423517efcb5f",
+    "url": "/spring-and-react/static/js/runtime-main.bb3cdbbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spring-and-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
